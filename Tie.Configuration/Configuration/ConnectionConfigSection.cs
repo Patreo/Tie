4306ffc10dd026d9c9055687f7f8d2ec92737eee
Pedro fernandes <pmcfernandes@gmail.com>
2015-11-16 16:14:09 +0000

    First Commit

---
+++ Tie.Configuration/Configuration/ConnectionConfigSection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Tie.Configuration
+{
+    public class ConnectionConfigSection : ConfigurationSection
+    {
+
+        /// <summary>
+        /// Gets or sets the default.
+        /// </summary>
+        /// <value>The default.</value>
+        [ConfigurationProperty("default", DefaultValue = "SqlProvider")]
+        public string Default
+        {
+            get { return (string)base["default"]; }
+            set { base["default"] = value; }
+        }
+
+        /// <summary>
+        /// Gets the providers.
+        /// </summary>
+        /// <value>The providers.</value>
+        [ConfigurationProperty("providers")]
+        public ProviderSettingsCollection Providers
+        {
+            get { return (ProviderSettingsCollection)base["providers"]; }
+        }
+    }
+}
---
+++ Tie.Configuration/Configuration/EmailConfigSection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Tie.Configuration
+{
+    public class EmailConfigSection : ConfigurationSection
+    {
+
+        /// <summary>
+        /// Gets or sets the default.
+        /// </summary>
+        /// <value>The default.</value>
+        [ConfigurationProperty("default", DefaultValue = "EmailProvider")]
+        public string Default
+        {
+            get { return (string)base["default"]; }
+            set { base["default"] = value; }
+        }
+
+        /// <summary>
+        /// Gets the providers.
+        /// </summary>
+        /// <value>The providers.</value>
+        [ConfigurationProperty("providers")]
+        public ProviderSettingsCollection Providers
+        {
+            get { return (ProviderSettingsCollection)base["providers"]; }
+        }
+    }
+}
